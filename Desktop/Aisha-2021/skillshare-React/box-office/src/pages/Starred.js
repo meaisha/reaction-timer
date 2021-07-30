3ced2d5692cc95e13c08235f9ad5c7bab69b5195
meaisha <meaisha@gmail.com>
2021-07-30 15:34:00 -0400

    Added layout management and created title

--- Desktop/Aisha-2021/skillshare-React/box-office/src/pages/Starred.js
+++ Desktop/Aisha-2021/skillshare-React/box-office/src/pages/Starred.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Starred = () => {
   return (
-    <div>This is starred page.</div>
+    <MainPageLayout>This is starred page.</MainPageLayout>
   )
 };
 
