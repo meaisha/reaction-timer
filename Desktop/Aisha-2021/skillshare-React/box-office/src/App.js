7d1eaf7fb6d34cf5d955cd2f607ab199f5c4e8bb
meaisha <meaisha@gmail.com>
2021-07-30 14:21:13 -0400

    added routing

--- Desktop/Aisha-2021/skillshare-React/box-office/src/App.js
+++ Desktop/Aisha-2021/skillshare-React/box-office/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <header>
-        hi
-      </header>
-    </div>
+  <Switch>
+    <Route exact path="/">
+      This is a home page.
+    </Route>
+
+    <Route exact path="/starred">
+      This is starred.
+    </Route>
+
+    <Route>This is 404 page.</Route>
+    </Switch>
   );
 }
 
