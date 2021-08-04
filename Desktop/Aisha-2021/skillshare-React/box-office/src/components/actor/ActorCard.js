9047b7293207781c4003fe881ec603c23cb860c7
meaisha <meaisha@gmail.com>
2021-08-04 19:33:30 -0400

    Installed Styled Components and created Styles for components

--- Desktop/Aisha-2021/skillshare-React/box-office/src/components/actor/ActorCard.js
+++ Desktop/Aisha-2021/skillshare-React/box-office/src/components/actor/ActorCard.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React from 'react';
+
+import { StyledActorCard } from './ActorCard.Styled';
 
 const ActorCard = ({ image, name, gender, country, birthday, deathday }) => {
   return (
-    <div>
-      <div>
+    <StyledActorCard>
+      <div className="img-wrapper">
         <img src={image} alt="actor" />
       </div>
 
       <h1>{name} {gender ? `${gender}` : null}</h1>
       <p>{country ? `Comes from ${country}` : 'No country known'}</p>
       {birthday ? <p>Born {birthday}</p> : null}
-      <p>{deathday ? `Died $(deathday)` : 'Alive'}</p>
-    </div>
+      <p className="deathday">{deathday ? `Died $(deathday)` : 'Alive'}</p>
+    </StyledActorCard>
   )
 }
 
