e4c2b9c7a65b064c04e183128a3709fcda624415
meaisha <meaisha@gmail.com>
2022-03-24 18:29:04 -0400

    signup form completed

--- tuts/web-form/src/components/SignupForm.vue
+++ tuts/web-form/src/components/SignupForm.vue
@@ -1,12 +1,70 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label for="">Email:</label>
-    <input type="email" required>
+    <input type="email" v-model="email" required>
+    <label for="">Password:</label>
+    <input type="password" v-model="password" required>
+    <div v-if="passwordError" class="error">{{passwordError}}</div>
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">
+        Web Developer
+      </option>
+      <option value="designer">Web Designer</option>
+    </select>
+    
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required>
+      <label>Accept terms and conditions</label>
+    </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
+    </div>
   </form>
 </template>
 <script>
 export default {
-  
+  data() {
+    return {
+      email: "",
+      password: "",
+      role: "designer",
+      terms: false,
+      tempSkill: "",
+      skills: [],
+      passwordError: ""
+    }
+  },
+  methods: {
+    addSkill(e) {
+      if(e.key === "," && this.tempSkill)
+      {
+        if(!this.skills.includes(this.tempSkill))
+          {
+            this.skills.push(this.tempSkill);
+            
+          }
+          this.tempSkill = "";
+      }
+    },
+
+    deleteSkill(skill) {
+      this.skills = this.skills.filter(item => { return skill !== item })
+    },
+
+    handleSubmit() {
+      console.log("submitted");
+      this.passwordError = this.password.length > 5 ? "" : "Password must be at least 6 chars long"
+    }
+  }
 }
 </script>
 <style>
@@ -27,7 +85,7 @@ export default {
     letter-spacing: 1px;
     font-weight: bold;
   }
-  input {
+  input, select {
     display: block;
     padding: 10px 6px;
     width: 100%;
@@ -36,6 +94,40 @@ export default {
     border-bottom: 1px solid #ddd;
     color: #555;
   }
+  input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+  }
+  .pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+  }
+   button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+  }
+  .submit {
+    text-align: center;
+  }
+  .error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
+  }
 </style>
-© 2022 GitHub, Inc.
-Terms
