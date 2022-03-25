6d5e5e9c9654ca29686b4808126b0d80482bf68a
meaisha <meaisha@gmail.com>
2022-03-25 12:48:25 -0400

    ninja jobs created

---
+++ tuts/ninja-jobs/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Home page</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque doloremque soluta eveniet, ab minima tenetur ipsa aliquam. Blanditiis, commodi sapiente vel, iusto et soluta debitis consequatur delectus quae inventore quo iste numquam facere provident perspiciatis enim! Dolorum quam minima repellendus debitis voluptas? Illo nobis dolore eos? Laborum dolorem, ipsum eius iure facere laudantium pariatur dolores molestiae aliquid quod illo temporibus non! Necessitatibus reiciendis repellendus ab nesciunt quas vero eveniet expedita ullam, neque odit culpa doloribus rem nemo maxime molestias explicabo deserunt. Odit numquam esse eligendi consequatur dolore eveniet vel inventore explicabo ullam, soluta unde autem dignissimos ad tempore pariatur? Architecto.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque doloremque soluta eveniet, ab minima tenetur ipsa aliquam. Blanditiis, commodi sapiente vel, iusto et soluta debitis consequatur delectus quae inventore quo iste numquam facere provident perspiciatis enim! Dolorum quam minima repellendus debitis voluptas? Illo nobis dolore eos? Laborum dolorem, ipsum eius iure facere laudantium pariatur dolores molestiae aliquid quod illo temporibus non! Necessitatibus reiciendis repellendus ab nesciunt quas vero eveniet expedita ullam, neque odit culpa doloribus rem nemo maxime molestias explicabo deserunt. Odit numquam esse eligendi consequatur dolore eveniet vel inventore explicabo ullam, soluta unde autem dignissimos ad tempore pariatur? Architecto.</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
